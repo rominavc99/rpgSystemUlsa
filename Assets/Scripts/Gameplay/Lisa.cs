3755839a23b335a68b23a61edad4d5f84229aa64
rominavc99 <procrom1999@gmail.com>
2022-03-25 13:34:59 -0700

    rgb system

---
+++ Assets/Scripts/Gameplay/Lisa.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lisa : Hero
+{
+
+    protected override void Movement()
+    {
+        base.Movement();
+
+        anim.SetFloat("move", movementValue);
+
+      
+    }
+}
